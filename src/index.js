a0310fcd4cab7a7aff9fd237f6cba02804696185
Aleksander Jaworski <aleck156@gmail.com>
2023-06-18 13:00:22 +0200

    move all components to index.js, remove redundant code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
@@ -16,3 +15,36 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <Avatar filename="./MAIN.png" />
+        <div className="data">
+          <Intro />
+          {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+          <SkillList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <div className="avatar">
+      <img src={props.filename} alt="main picture" />
+    </div>
+  );
+}
+
+function Intro() {
+  return <div>Intro Placeholder</div>;
+}
+
+function SkillList() {
+  return <div>SkillList Placeholder</div>;
+}
